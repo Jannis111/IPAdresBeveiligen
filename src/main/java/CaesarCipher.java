0fd7f5d52edad28945f2a0b2478d52ea2d34c5d4
Jannis Geerts <JA.Geerts@student.han.nl>
2025-05-29 13:18:44 +0200

    Symmetric CaesarCipher

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,30 +1,13 @@
 import java.util.ArrayList;
 
 public class CaesarCipher {
-
-    public String encrypt(String text, int shift) {
+    public String caesar(String text, int shift) {
         StringBuilder encrypted = new StringBuilder();
 
         for (char c : text.toCharArray()) {
             if (Character.isLetter(c)) {
                 char base = Character.isUpperCase(c) ? 'A' : 'a';
                 char shifted = (char) ((c - base + shift) % 26 + base);
-                encrypted.append(shifted);
-            } else {
-                encrypted.append(c);
-            }
-        }
-
-        return encrypted.toString();
-    }
-
-    public String decrypt(String text, int shift) {
-        StringBuilder encrypted = new StringBuilder();
-
-        for (char c : text.toCharArray()) {
-            if (Character.isLetter(c)) {
-                char base = Character.isUpperCase(c) ? 'A' : 'a';
-                char shifted = (char) ((c - base - shift) % 26 + base);
                 encrypted.append(shifted);
             } else {
                 encrypted.append(c);
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         CaesarCipher cipher = new CaesarCipher();
-        String text = cipher.encrypt("abcd", 1);
+        String text = cipher.caesar("abcd", 1);
         System.out.println(text);
-        text = cipher.decrypt(text, 1);
+        text = cipher.caesar(text, -1);
         System.out.println(text);
     }
 }
